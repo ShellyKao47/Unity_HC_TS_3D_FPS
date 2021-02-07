3807dc8ea9bd51a3fc478c461fd4d5a2a9377cfc
KID <50666207+KID421@users.noreply.github.com>
2021-02-07 17:00:36 +0800

    添加房子碰撞

---
+++ 3D_FPS/Assets/Scripts/FPSController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    [Header("移動"), Range(0, 2000)]
+    public float speed;
+    [Header("旋轉"), Range(0, 2000)]
+    public float turn;
+
+    private Animator ani;
+    private Rigidbody rig;
+
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void Move()
+    {
+        float v = Input.GetAxis("Vertical");    // W S 上 下 - 前進 1，後退 -1
+        float h = Input.GetAxis("Horizontal");  // A D 左 右
+
+        rig.MovePosition(transform.position + transform.forward * v * speed * Time.deltaTime + transform.right * h * speed * Time.deltaTime);
+    }
+}
