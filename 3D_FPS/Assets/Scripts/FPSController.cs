4fe0e0347bc2bbafae6948be0ab5b1a5b88c04f5
KID <50666207+KID421@users.noreply.github.com>
2021-02-21 17:03:12 +0800

    開槍、換彈夾動畫與功能

--- 3D_FPS/Assets/Scripts/FPSController.cs
+++ 3D_FPS/Assets/Scripts/FPSController.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class FPSController : MonoBehaviour
 {
@@ -27,8 +29,30 @@ public class FPSController : MonoBehaviour
     public int bulletCurrent = 30;
     [Header("子彈總數")]
     public int bulletTotal = 150;
+    [Header("彈夾裝填子彈數量")]
+    public int bulletClip = 30;
     [Header("子彈速度")]
     public int bulletSpeed = 450;
+    [Header("文字：子彈目前數量")]
+    public Text textBulletCurrent;
+    [Header("文字：子彈總數")]
+    public Text textBulletTotal;
+    [Header("補充子彈時間"), Range(0, 5)]
+    public float addBulletTime = 1;
+    [Header("開槍音效")]
+    public AudioClip soundFire;
+    [Header("換彈夾音效")]
+    public AudioClip soundAddBullet;
+    [Header("開槍間隔時間"), Range(0f, 1f)]
+    public float fireInterval = 0.1f;
+
+    private AudioSource aud;
+    private float timer;
+
+    /// <summary>
+    /// 是否在補充子彈
+    /// </summary>
+    private bool isAddBullet;
     #endregion
 
     private void OnDrawGizmos()
@@ -42,6 +66,7 @@ public class FPSController : MonoBehaviour
         Cursor.visible = false;             // 隱藏滑鼠
         ani = GetComponent<Animator>();
         rig = GetComponent<Rigidbody>();
+        aud = GetComponent<AudioSource>();
     }
 
     private void Update()
@@ -49,6 +74,7 @@ public class FPSController : MonoBehaviour
         Move();
         Jump();
         Fire();
+        AddBullet();
     }
 
     /// <summary>
@@ -56,14 +82,66 @@ public class FPSController : MonoBehaviour
     /// </summary>
     private void Fire()
     {
-        // 按下左鍵
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        // 按下左鍵 並且 子彈 > 0 並且 不是在補充子彈
+        if (Input.GetKey(KeyCode.Mouse0) && bulletCurrent > 0 && !isAddBullet)
         {
-            // 暫存子彈 = 生成(物件，座標，角度)
-            GameObject temp = Instantiate(bullet, pointFire.position, pointFire.rotation);
-            // 暫存子彈.取得剛體.添加推力(生成點的前方 * 速度)
-            temp.GetComponent<Rigidbody>().AddForce(pointFire.up * bulletSpeed);
+            if (timer >= fireInterval)
+            {
+                ani.SetTrigger("開槍觸發");
+                timer = 0;
+                aud.PlayOneShot(soundFire, Random.Range(0.7f, 1.2f));
+                // 扣除子彈並更新介面
+                bulletCurrent--;
+                textBulletCurrent.text = bulletCurrent.ToString();
+                // 暫存子彈 = 生成(物件，座標，角度)
+                GameObject temp = Instantiate(bullet, pointFire.position, pointFire.rotation);
+                // 暫存子彈.取得剛體.添加推力(生成點的前方 * 速度)
+                temp.GetComponent<Rigidbody>().AddForce(pointFire.up * bulletSpeed);
+            }
+            else timer += Time.deltaTime;
         }
+    }
+
+    /// <summary>
+    /// 補充子彈
+    /// </summary>
+    private void AddBullet()
+    {
+        // 條件
+        // 1. 按 R
+        // 2. 不是在補充子彈
+        // 3. 子彈總數 大於 0
+        // 4. 目前子彈 小於 彈夾
+        if (Input.GetKeyDown(KeyCode.R) && !isAddBullet && bulletTotal > 0 && bulletCurrent < bulletClip) StartCoroutine(AddBulletDelay());
+    }
+
+    /// <summary>
+    /// 補充子彈協程
+    /// </summary>
+    private IEnumerator AddBulletDelay()
+    {
+        ani.SetTrigger("換彈夾觸發");
+        aud.PlayOneShot(soundAddBullet, Random.Range(0.8f, 1.1f));
+
+        isAddBullet = true;
+        yield return new WaitForSeconds(addBulletTime);
+        isAddBullet = false;
+
+        int add = bulletClip - bulletCurrent;               // 計算補幾顆子彈
+
+        if (bulletTotal >= add)                             // 如果總數 大於 要補充的子彈
+        {
+            bulletCurrent += add;
+            bulletTotal -= add;
+        }
+        else                                                // 否則 將剩餘總數 補充過來
+        {
+            bulletCurrent += bulletTotal;
+            bulletTotal = 0;
+        }
+
+        textBulletCurrent.text = bulletCurrent.ToString();
+        textBulletTotal.text = bulletTotal.ToString();
     }
 
     /// <summary>
